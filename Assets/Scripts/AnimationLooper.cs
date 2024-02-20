e42ee87bfc721f294b6d7d1007eb6defd5e6d196
Marqe <marqe.mywords@gmail.com>
2024-02-20 16:35:40 +0100

    created timelines, polished environment, added master timeline through doors of house

--- Assets/Scripts/AnimationLooper.cs
+++ Assets/Scripts/AnimationLooper.cs
@@ -1,15 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Timeline;
 
-
-
-public class PlayerRail : MonoBehaviour
+public class AnimationLooper : MonoBehaviour
 {
-
-
-    TimelineClip myTimeLine;
     // Start is called before the first frame update
     void Start()
     {
